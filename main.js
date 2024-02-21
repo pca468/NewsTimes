89dfe024a52de42a88380c6a64f6674ad7b7060f
DongHyun <pca468@naver.com>
2024-02-22 01:27:48 +0900

    news Render()

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_KEY = ``
-let news = []
+let newsList = [];
 
 const getLatesNews = async() => {
     const url = new URL(`https://kimsnewstimes.netlify.app/top-headlines`
@@ -9,12 +9,37 @@ console.log("uuu", url)
 
 const response = await fetch(url); // await => 이 함수를 일시정지 해줄래? 그 다음 call stack이 없을 때 다시 실행
 const data = await response.json() // json은 객체를 텍스트화 시킨것
-news = data.articles
+newsList = data.articles
+render();
 
 console.log("rrr", response) // pending 아직 결과가 안나왔다. 기다려줘...!
-console.log("dddd", news)
+console.log("dddd", newsList)
 };
 
+const render = () => {
+    const newsHTML = newsList.map(
+        news => `
+            <div class = "row news">
+                <div class = "col-lg-4">
+                    <img class = "news-img-size" 
+                    src=${news.urlToImage}>
+                </div>
+                    <div class ="col-lg-8">
+                        <h2>${news.title}</h2>
+                        <p>${news.description}</p>
+                        <div>
+                            ${news.source.name} * ${news.publishedAt}
+                        </div>
+                    </div>
+            </div>`
+            )
+            .join("");
+        console.log("HTML", newsHTML)
+   
+
+    document.getElementById('news-board').innerHTML=newsHTML
+}
+render();
 getLatesNews();
 
 
