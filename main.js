956a173abbb58aa162b476a568dbbc2db9ae65f6
DongHyun <pca468@naver.com>
2024-02-24 22:02:04 +0900

    pagination

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ menus.forEach(menu =>
     menu.addEventListener("click", (event) => getNewsCategory(event))
     );
 
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize = 5
+
 const getNews = async() => {
     try {
+        url.searchParams.set("page", page); // => &page = page
+        url.searchParams.set("pageSize", pageSize);
+
         const response = await fetch(url); // await => 이 함수를 일시정지 해줄래? 그 다음 call stack이 없을 때 다시 실행
         const data = await response.json(); // json은 객체를 텍스트화 시킨것
         if(response.status === 200) {
@@ -14,7 +22,9 @@ const getNews = async() => {
                 throw new Error("No result for this search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults
             render();
+            paginationRender();
         } else {
             throw new Error(data.message)
         }
@@ -112,6 +122,43 @@ const errorRender = (errorMessage) => {
     `;
     document.getElementById("news-board").innerHTML = errotHTML;
 }
+
+const paginationRender = () => {
+    // totalResult,
+    // page,
+    // pageSize
+    // groupSize
+    // totalpages
+    const totalPages = Math.ceil(totalResults / pageSize);
+    // pageGroup
+    const pageGroup = Math.ceil(page / groupSize);
+    // lastPage
+    const lastPage = pageGroup * groupSize;
+    // 마지막 페이지 그룹이 그룹사이즈보다 작다? lastpage = totalpage
+    if(lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+
+
+    // firstPage
+    const firstPage = 
+        lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+    let paginationHTML = ``;
+
+    for(let i = firstPage; i <= lastPage; i++){
+        paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" onclick = "moveToPage(${i})"><a class="page-link">${i}</a></li>`
+    }
+
+    document.querySelector(".pagination").innerHTML = paginationHTML
+}
+
+const moveToPage = (pageNum) => {
+    console.log("moveTOPage", pageNum);
+    page = pageNum;
+    getNews();
+
+};
 
 render();
 getLatesNews();
